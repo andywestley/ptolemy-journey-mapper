a1287d0d26d06a849fb9500e667da529b3b6a0f7
andywestley <andy@andrewwestley.co.uk>
2026-07-08 19:49:31 +0100

    Bump cache-buster version to v1.0.5 to clear cached dashboard.js

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -3,7 +3,7 @@
 
     <?php if (isset($extraJs)): ?>
         <?php foreach ($extraJs as $jsFile): ?>
-            <script src="js/<?php echo $jsFile; ?>?v=1.0.4"></script>
+            <script src="js/<?php echo $jsFile; ?>?v=1.0.5"></script>
         <?php endforeach; ?>
     <?php endif; ?>
 </body>
